61bfc8f9824458364e0ccee02f34b4f4605c780b
Wenmoux <45694869+Wenmoux@users.noreply.github.com>
2020-11-09 22:21:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
      async function start(task) {
             console.log('开始签到任务')
             for (let i = 0; i < task.length; i++) {                                      
-                await taskarr[k]() 
+                await task[i]() 
             }            
             console.log('任务执行完毕')
         }
